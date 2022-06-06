242cf276d451559c5d64a3ab4f079d03e6a9a20c
= <aashish.kumar@pythonmate.com>
2022-06-06 22:32:38 +0530

    Initial commit

---
+++ app/src/main/java/com/yt/commoncodeandroid/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.yt.commoncodeandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yt.commoncodeandroid.ui.theme.CommonCodeAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CommonCodeAndroidTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   Button(onClick = {}) {
+
+                   }
+                }
+            }
+        }
+    }
+}
